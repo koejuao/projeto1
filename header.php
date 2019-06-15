3f080361a21c900fecb005164591bb1262592687
koejuao <santana.br@outlook.com.br>
2019-06-15 19:54:04 -0300

    modo mobile home

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1">
- 
     <?php wp_head(); ?>
-
   </head>
   <body>
   	<header class="menu">
